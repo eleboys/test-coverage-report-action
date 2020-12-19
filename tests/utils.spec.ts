70b6c9365afd9ea4ccd89f69d3c20724309d0ae5
Nima Baghdadi <bg.nima@gmail.com>
2020-12-19 23:52:22 +0100

    added unittests for utils

--- tests/utils.spec.ts
+++ tests/utils.spec.ts
@@ -4,4 +4,13 @@ describe("test addReport method", () => {
     it("should exist", () => {
         expect(addReport).toBeTruthy();
     });
+
+    it("should return valid object if both sides are null", () => {
+        const res = addReport(null, null);
+        expect(res).toBeTruthy();
+        expect(res.branches).not.toBeFalsy();
+        expect(res.functions).not.toBeFalsy();
+        expect(res.lines).not.toBeFalsy();
+        expect(res.statements).not.toBeFalsy();
+    })
 })
