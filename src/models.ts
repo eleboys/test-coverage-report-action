79c80f59c763ca590e139a547212452a7a8dc6cd
Nima Baghdadi <bg.nima@gmail.com>
2020-12-19 21:38:58 +0100

    editorconfig

--- src/models.ts
+++ src/models.ts
@@ -1,26 +1,26 @@
 export interface ReportItem {
-  fileName: string;
-  report: Report;
+    fileName: string;
+    report: Report;
 }
 
 export interface Report {
-  lines: CoverageReport;
-  functions: CoverageReport;
-  statements: CoverageReport;
-  branches: CoverageReport;
+    lines: CoverageReport;
+    functions: CoverageReport;
+    statements: CoverageReport;
+    branches: CoverageReport;
 }
 
 export interface CoverageReport {
-  total: number;
-  covered: number;
-  skipped: number;
-  pct: number;
+    total: number;
+    covered: number;
+    skipped: number;
+    pct: number;
 }
 
 export interface PullRequest {
-  owner: string;
-  repo: string;
-  issueNumber: number;
-  base: string;
-  head: string;
+    owner: string;
+    repo: string;
+    issueNumber: number;
+    base: string;
+    head: string;
 }
